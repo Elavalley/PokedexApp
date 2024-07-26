68c0556703ecf27a719bc02ee15c4df269f4fbfb
Ethan <lavaet01@gettysburg.edu>
2024-07-26 14:54:54 -0400

    finished pokemonpage?

--- PokedexApp/obj/Debug/net8.0-ios/iossimulator-arm64/PokedexApp.AssemblyInfo.cs
+++ PokedexApp/obj/Debug/net8.0-ios/iossimulator-arm64/PokedexApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+42ab5a40b9b9a3526d2b22da6c8abe19ecc183a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f835249407a569522eeadf2d4dae18816d17c840")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
