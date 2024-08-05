46025a2e361626348222c51af22c77f4b9e214a5
Ethan Lavalley <167122711+Elavalley@users.noreply.github.com>
2024-08-05 15:01:25 -0400

    d

--- PokedexApp/PokemonPage.xaml.cs
+++ PokedexApp/PokemonPage.xaml.cs
@@ -1,282 +1,241 @@
-using MongoDB.Bson;
-using MongoDB.Driver;
-using System;
+using System.Runtime.CompilerServices;
 using System.ComponentModel;
+using MongoDB.Driver;
+using MongoDB.Bson;
 using System.Threading.Tasks;
+using System;
 
-public class PokemonPage
+namespace PokedexApp;
+
+
+public partial class PokemonPage : ContentPage
 {
-    private static string userInput = "";
-    public static string GetUserInput() { return userInput; }
-
-    public PokemonPage()
+    
+    public PokemonPage(string searchText)
     {
-        userInput = Console.ReadLine();
-        PokemonDetailViewModel viewModel = new PokemonDetailViewModel();
-        viewModel.LoadPokemonDataAsync(userInput);
+        InitializeComponent();
+        var viewModel = new PokemonDetailViewModel();
+        BindingContext = viewModel;
+        viewModel.LoadPokemonDataAsync(searchText);
+
+    }
+    public class PokemonDetailViewModel
+    {
+    
+    #region attribute binders
+    
+    private int number;
+    public int Number
+    {
+    get => number;
+    set { number = value; OnPropertyChanged(); }
     }
 
-    public class PokemonDetailViewModel : INotifyPropertyChanged
+    private string name;
+    public string Name
     {
+    get => name;
+    set { name = value; OnPropertyChanged(); }
+    }
+    
+    
+    private int _hp;
+    public int HP
+    {
+        get => _hp;
+        set
+        {
+            _hp = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(HPProgress));
+        }
+    }
+    public double HPProgress => HP / 200.0;
+    
+    
+    private double _atk;
+    public double Attack
+    {
+        get => _atk;
+        set
+        {
+            _atk = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(AttackProgress));
+        }
+    }
+    public double AttackProgress => Attack / 200.0;
+    
+    
+    private double _def;
+    public double Defense
+    {
+        get => _def;
+        set
+        {
+            _def = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(DefenseProgress));
+        }
+    }
+    public double DefenseProgress => Defense / 200.0;
+    
+    private double _spatk;
+    public double SpAtk
+    {
+        get => _spatk;
+        set
+        {
+            _spatk = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(SpAtkProgress));
+        }
+    }
+    public double SpAtkProgress => SpAtk / 200.0;
+
+
+    private double _spdef;
+    public double SpDef
+    {
+        get => _spdef;
+        set
+        {
+            _spdef = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(SpDefProgress));
+        }
+    }
+    public double SpDefProgress => SpDef / 200.0;
+
+
+    private double _spd;
+    public double Speed
+    {
+        get => _spd;
+        set
+        {
+            _spd = value;
+            
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(SpeedProgress));
+        }
+    }
+    public double SpeedProgress => Speed / 200.0;
+    
+
+    private string type1;
+    public string Type1
+    {
+    get => type1;
+    set { type1 = value; OnPropertyChanged(); }
+    }
+
+    private string type2;
+    public string Type2
+    {
+    get => type2;
+    set { type2 = value; OnPropertyChanged(); }
+    }
+
+    private string height;
+    public string Height
+    {
+    get => height;
+    set { height = value; OnPropertyChanged(); }
+    }
+
+    private string weight;
+    public string Weight
+    {
+    get => weight;
+    set { weight = value; OnPropertyChanged(); }
+    }
+
+    private string baseTotal;
+    public string BaseTotal
+    {
+    get => baseTotal;
+    set { baseTotal = value; OnPropertyChanged(); }
+    }
+        #endregion
+
+
+
+
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private int number;
-        public int Number
-        {
-            get { return number; }
-            set
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public async Task LoadPokemonDataAsync(string pokemonName)
+    {
+        try
             {
-                if (number != value)
-                {
-                    int oldNumber = number;
-                    number = value;
-                    OnPropertyChanged("Number");
-                }
-            }
-        }
+            Console.WriteLine($"Attempting to connect to database and search for {pokemonName}");
+            
+            string connectionString = "mongodb+srv://tkhanpsn:Pokegocult@pokedex.mayccu6.mongodb.net/";
+            string databaseName = "Pokedex";
+            string collectionName = "Pokemon";
 
-        private string name;
-        public string Name
-        {
-            get { return name; }
-            set
-            {
-                if (name != value)
-                {
-                    string oldName = name;
-                    name = value;
-                    OnPropertyChanged("Name");
-                }
-            }
-        }
+            var client = new MongoClient(connectionString);
+            Console.WriteLine("MongoClient created successfully");
 
-        private int hp;
-        public int HP
-        {
-            get { return hp; }
-            set
-            {
-                if (hp != value)
-                {
-                    int oldHP = hp;
-                    hp = value;
-                    OnPropertyChanged("HP");
-                    OnPropertyChanged("HPProgress");
-                }
-            }
-        }
-        public double HPProgress => hp / 200.0;
+            var database = client.GetDatabase(databaseName);
+            Console.WriteLine("Database retrieved successfully");
+            
 
-        private double atk;
-        public double Attack
-        {
-            get { return atk; }
-            set
-            {
-                if (atk != value)
-                {
-                    double oldAtk = atk;
-                    atk = value;
-                    OnPropertyChanged("Attack");
-                    OnPropertyChanged("AttackProgress");
-                }
-            }
-        }
-        public double AttackProgress => atk / 200.0;
+            var collection = database.GetCollection<PokemonDetailViewModel>(collectionName);
+            Console.WriteLine("Collection retrieved successfully");
 
-        private double def;
-        public double Defense
-        {
-            get { return def; }
-            set
-            {
-                if (def != value)
-                {
-                    double oldDef = def;
-                    def = value;
-                    OnPropertyChanged("Defense");
-                    OnPropertyChanged("DefenseProgress");
-                }
-            }
-        }
-        public double DefenseProgress => def / 200.0;
 
-        private double spatk;
-        public double SpAtk
-        {
-            get { return spatk; }
-            set
-            {
-                if (spatk != value)
-                {
-                    double oldSpAtk = spatk;
-                    spatk = value;
-                    OnPropertyChanged("SpAtk");
-                    OnPropertyChanged("SpAtkProgress");
-                }
-            }
-        }
-        public double SpAtkProgress => spatk / 200.0;
+            //Use Builders<PokemonViewModel>.Filter instead of Builders<BsonDocument>.Filter
+            var filter = Builders<PokemonDetailViewModel>.Filter.Eq($" Name", "{pokemonName}");
+            var pokemon = await collection.Find(filter).FirstOrDefaultAsync();
+            Console.WriteLine("Find operation completed");
 
-        private double spdef;
-        public double SpDef
-        {
-            get { return spdef; }
-            set
-            {
-                if (spdef != value)
-                {
-                    double oldSpDef = spdef;
-                    spdef = value;
-                    OnPropertyChanged("SpDef");
-                    OnPropertyChanged("SpDefProgress");
-                }
-            }
-        }
-        public double SpDefProgress => spdef / 200.0;
-
-        private double spd;
-        public double Speed
-        {
-            get { return spd; }
-            set
-            {
-                if (spd != value)
-                {
-                    double oldSpd = spd;
-                    spd = value;
-                    OnPropertyChanged("Speed");
-                    OnPropertyChanged("SpeedProgress");
-                }
-            }
-        }
-        public double SpeedProgress => spd / 200.0;
-
-        private string type1;
-        public string Type1
-        {
-            get { return type1; }
-            set
-            {
-                if (type1 != value)
-                {
-                    string oldType1 = type1;
-                    type1 = value;
-                    OnPropertyChanged("Type1");
-                }
-            }
-        }
-
-        private string type2;
-        public string Type2
-        {
-            get { return type2; }
-            set
-            {
-                if (type2 != value)
-                {
-                    string oldType2 = type2;
-                    type2 = value;
-                    OnPropertyChanged("Type2");
-                }
-            }
-        }
-
-        private string height;
-        public string Height
-        {
-            get { return height; }
-            set
-            {
-                if (height != value)
-                {
-                    string oldHeight = height;
-                    height = value;
-                    OnPropertyChanged("Height");
-                }
-            }
-        }
-
-        private string weight;
-        public string Weight
-        {
-            get { return weight; }
-            set
-            {
-                if (weight != value)
-                {
-                    string oldWeight = weight;
-                    weight = value;
-                    OnPropertyChanged("Weight");
-                }
-            }
-        }
-
-        private string baseTotal;
-        public string BaseTotal
-        {
-            get { return baseTotal; }
-            set
-            {
-                if (baseTotal != value)
-                {
-                    string oldBaseTotal = baseTotal;
-                    baseTotal = value;
-                    OnPropertyChanged("BaseTotal");
-                }
-            }
-        }
-
-        protected void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        public async void LoadPokemonDataAsync(string pokemonName)
-        {
-            try
-            {
-                string connectionString = "mongodb+srv://tkhanpsn:<Pokegocult>@pokedex.mayccu6.mongodb.net/";
-                string databaseName = "Pokedex";
-                string collectionName = "Pokemon";
-
-                var client = new MongoClient(connectionString);
-                var database = client.GetDatabase(databaseName);
-                var collection = database.GetCollection<BsonDocument>(collectionName);
-
-                var filter = Builders<BsonDocument>.Filter.Eq("Name", PokemonPage.GetUserInput());
-                var pokemon = await collection.Find(filter).FirstOrDefaultAsync();
 
                 if (pokemon != null)
                 {
-                    Number = pokemon["Number"].AsInt32;
-                    Name = pokemon["Name"].AsString;
-                    Attack = pokemon["Attack"].AsDouble;
-                    Defense = pokemon["Defense"].AsDouble;
-                    Speed = pokemon["Speed"].AsDouble;
-                    HP = pokemon["HP"].AsInt32;
-                    Type1 = pokemon["Type1"].AsString;
-                    Type2 = pokemon["Type2"].AsString;
-                    Weight = pokemon["Weight"].AsString;
-                    Height = pokemon["Height"].AsString;
-                    BaseTotal = pokemon["BaseTotal"].AsString;
+                    // Map database fields to properties
+                    Console.WriteLine($"Pokemon Found: {pokemonName}");
+                    Number = pokemon.Number;
+                    Name = pokemon.Name;
+                    Attack = pokemon.Attack;
+                    Defense = pokemon.Defense;
+                    Speed = pokemon.Speed;
+                    HP = pokemon.HP;
+                    Type1 = pokemon.Type1;
+                    Type2 = pokemon.Type2;
+                    Weight = pokemon.Weight;
+                    Height = pokemon.Height;
+                    BaseTotal = pokemon.BaseTotal;
+
+                    Console.WriteLine("Properties assigned successfully");
+                    OnPropertyChanged(nameof(Number));
+                    OnPropertyChanged(nameof(Name));
+                    OnPropertyChanged(nameof(Attack));
+                    OnPropertyChanged(nameof(Defense));
+                    OnPropertyChanged(nameof(Speed));
+                    OnPropertyChanged(nameof(HP));
+                    OnPropertyChanged(nameof(Type1));
+                    OnPropertyChanged(nameof(Type2));
+                    OnPropertyChanged(nameof(Weight));
+                    OnPropertyChanged(nameof(Height));
+                    OnPropertyChanged(nameof(BaseTotal));
+
+                    // Add other properties you need to set
                 }
                 else
-                {
-                    Console.WriteLine("Pokemon Not Found");
-                }
+                    {
+                    await Application.Current.MainPage.DisplayAlert("Error", "Pokemon Not Found", "OK");
+                    }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("An Error occurred: " + ex.Message);
+            await Application.Current.MainPage.DisplayAlert("Error", $"An error occurred: {ex.Message}", "OK");
             }
         }
     }
-
-    public static void Main(string[] args)
-    {
-        new PokemonPage();
-    }
 }
-
-
