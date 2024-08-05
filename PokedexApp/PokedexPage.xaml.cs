4f38c6d09f379fe33ad53b97a4768dfb3d6ab73a
Ethan Lavalley <167122711+Elavalley@users.noreply.github.com>
2024-08-05 14:55:42 -0400

    Update PokedexPage.xaml.cs

--- PokedexApp/PokedexPage.xaml.cs
+++ PokedexApp/PokedexPage.xaml.cs
@@ -1,20 +1,31 @@
-namespace PokedexApp;
-public partial class PokedexPage : ContentPage 
+namespace PokedexApp
+{
+    public partial class PokedexPage : ContentPage
     {
-
         private static string userInput = "";
         public static string GetUserInput() { return userInput; }
+        
         public PokedexPage()
-        {        
+        {
             InitializeComponent();
         }
-        private async void OnSearchPressed(object sender, TextChangedEventArgs e)
+
+        private async void OnSearchPressed(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new PokemonPage(userInput));
+            if (!string.IsNullOrEmpty(userInput))
+            {
+                await Navigation.PushAsync(new PokemonPage(userInput));
+            }
         }
+
+        private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+        {
+            userInput = e.NewTextValue;
+        }
+
         private async void OnStartClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new PokemonPage("Bulbasaur"));  
         }
-
+    }
 }
